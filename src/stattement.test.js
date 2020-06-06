90db2c59b46b66001b6a3f81b1b415374f5a7b4c
GPAzevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-06-05 22:19:51 -0300

    Adding a E2E Test

---
+++ src/stattement.test.js
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//   Refactoring Chapter 1
+//
+///////////////////////////////////////////////////////////////////////////////
+
+const  { statement } = require ('./statement');
+
+const result = `Statement for BigCo
+   Hamlet: $650.00 (55 seats)
+   As You Like It: $580.00 (35 seats)
+   Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`;
+
+// Get the plays
+var plays = require('./plays.json');
+
+// Get the invoices
+var invoices = require('./invoices.json');
+
+let invoice_msg
+for (let inv of invoices) {
+    invoice_msg = statement (inv, plays);
+}
+
+test ('Total Statement', () => {
+    expect(invoice_msg).toBe(result);
+});
