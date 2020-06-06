e23f01a414cdfb02c382a78a04cc8c54cc845dcd
GPAzevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-06-06 01:49:39 -0300

    Adding a function that Formats a numbers according to its locale

--- src/statement.js
+++ src/statement.js
@@ -5,27 +5,22 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 function statement (invoice, plays) {
+    let locale = "en-US";  // Its possible any locales
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US", 
-            {
-                style: "currency", currency: "USD", 
-                minimumFractionDigits: 2
-            }).format;
 
     for (let perf of invoice.performances) {
         let thisAmount = amountFor(perf);
-        let play = playFor(perf);
 
         // add volume credits
         volumeCredits += volumeCreditsFor (perf);
 
         // print line for this order
-        result += `   ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        result += `   ${playFor(perf).name}: ${format(thisAmount/100, locale)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${format(totalAmount/100, locale)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
@@ -71,6 +66,23 @@ function statement (invoice, plays) {
         return volumeCredits;
     };
 
+    // Formats a numbers according to its locale
+    function format(aNumber, locale) {
+        let result;
+        switch (locale) {
+            case "en-US":
+                result = new Intl.NumberFormat("en-US", 
+                {
+                    style: "currency", currency: "USD", 
+                    minimumFractionDigits: 2
+                }).format(aNumber);
+                break;
+        
+            default:
+                break;
+        }
+        return result;
+    }
 }
 
 module.exports = {statement}
