ad8ee51660f4ac0f94958d6f68e40f40179df4a8
GPAzevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-06-07 10:56:25 -0300

    New class PerformanceCalculator which calculates the amount and volume credits for the invoice

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -5,6 +5,47 @@
 ///////////////////////////////////////////////////////////////////////////////
 module.exports = {createStatementData}
 
+class PerformanceCalculator {
+    constructor (aPerformance, aPlay) {
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount () {
+        let thisAmount = 0;
+
+        switch (this.play.type) {
+            case "tragedy":
+                thisAmount += 40000;
+                if (this.performance.audience > 30) {
+                    thisAmount += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (this.performance.audience > 20) {
+                    thisAmount += 10000 + 500 * (this.performance.audience - 20);
+                }
+                thisAmount += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error (`Unknow type: ${this.performance.play.type}`);        
+        };
+        return (thisAmount);
+    }
+
+    get volumeCredits () {
+        // add volume credits
+        let volumeCredits = Math.max(this.performance.audience - 30, 0);
+
+        //add extra credit for every ten comedy attendees
+        if ("comedy" === this.play.type) {
+            volumeCredits += Math.floor(this.performance.audience / 5);  
+        }
+        return volumeCredits;
+    }
+}
+
 function createStatementData (invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -15,10 +56,11 @@ function createStatementData (invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator (aPerformance, playFor(aPerformance));
         let result = Object.assign({}, aPerformance);
-        result.play = playFor (result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
@@ -27,43 +69,8 @@ function createStatementData (invoice, plays) {
         return (plays[aPerformance.playID]);
     };
 
-    // Returns the amount for a performance
-    function amountFor(aPerformance) {
-        let thisAmount = 0;
 
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                thisAmount += 40000;
-                if (aPerformance.audience > 30) {
-                    thisAmount += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (aPerformance.audience > 20) {
-                    thisAmount += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                thisAmount += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error (`Unknow type: ${aPerformance.play.type}`);        
-        };
-        return (thisAmount);
-    }
-
-    // Returns the volume credits
-    function volumeCreditsFor(aPerformance) {
-        // add volume credits
-        let volumeCredits = Math.max(aPerformance.audience - 30, 0);
-
-        //add extra credit for every ten comedy attendees
-        if ("comedy" === aPerformance.play.type) {
-            volumeCredits += Math.floor(aPerformance.audience / 5);  
-        }
-        return volumeCredits;
-     };
-
-     function totalVolumeCredits (data) {
+    function totalVolumeCredits (data) {
        return data.performances.reduce((total, p) => total + p.volumeCredits,0);
     }
 
